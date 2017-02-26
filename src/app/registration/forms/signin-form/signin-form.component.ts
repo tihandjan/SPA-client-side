5e5e76f3778755047ddcaaeae396b8a500e86c84
tihandjan <tihandjan198921@gmail.com>
2017-02-26 22:01:28 +0300

    add signin/signup forms

---
+++ src/app/registration/forms/signin-form/signin-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signin-form',
+  templateUrl: './signin-form.component.html',
+  styleUrls: ['./signin-form.component.css']
+})
+export class SigninFormComponent implements OnInit {
+
+  signInObject: any = {
+    email: '',
+    password: ''
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
