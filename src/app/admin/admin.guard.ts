54a6cd21f8cbaaf5ece8e09a81c7309832e66521
tihandjan <tihandjan198921@gmail.com>
2017-02-28 03:19:18 +0300

    add injectable to admin guard

--- src/app/admin/admin.guard.ts
+++ src/app/admin/admin.guard.ts
@@ -1,6 +1,8 @@
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AdminGuard implements CanActivate {
     canActivate(router: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
         return confirm('Are you sure?');
